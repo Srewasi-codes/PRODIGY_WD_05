061b6d8d7ffa7596912d43ee909e1e81c0ea5196
Srewasi-codes <srewasitalukdar.adtu@gmail.com>
2024-10-03 11:14:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,32 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b45e96323fmshf5e4bc50d3b8a90p1e091djsnfb21cde4eb87',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
-const getWeather = (city) =>{
-	cityName.innerHTML = city
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+ city, options)
-	.then(response => response.json())
-	.then(response => {
+const apiKey = '7c32f97e642c5d2a4388094da250574e'; // Your OpenWeatherMap API key
 
+const getWeather = (city) => {
+    cityName.innerHTML = city;
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
 
-
-
-		console.log(response) 
-		
-		// cloud_pct.innerHTML = response.cloud_pct
-		temp.innerHTML = response.temp
-		temp2.innerHTML = response.temp
-		feels_like.innerHTML = response.feels_like
-		humidity.innerHTML = response.humidity
-		humidity2.innerHTML = response.humidity
-		min_temp.innerHTML = response.min_temp
-		max_temp.innerHTML = response.max_temp
-		wind_speed.innerHTML = response.wind_speed
-		wind_speed2.innerHTML = response.wind_speed
-		wind_degrees.innerHTML = response.wind_degrees
-		sunrise.innerHTML = response.sunrise
-		sunset.innerHTML = response.sunset
-	})
-	.catch(err => console.error(err));
+            temp.innerHTML = response.main.temp || "N/A"; 
+            temp2.innerHTML = response.main.temp || "N/A"; 
+            feels_like.innerHTML = response.main.feels_like || "N/A"; 
+            humidity.innerHTML = response.main.humidity || "N/A"; 
+            humidity2.innerHTML = response.main.humidity || "N/A"; 
+            min_temp.innerHTML = response.main.temp_min || "N/A"; 
+            max_temp.innerHTML = response.main.temp_max || "N/A"; 
+            wind_speed.innerHTML = response.wind.speed || "N/A"; 
+            wind_speed2.innerHTML = response.wind.speed || "N/A";
+            wind_degrees.innerHTML = response.wind.deg || "N/A"; 
+            sunrise.innerHTML = new Date(response.sys.sunrise * 1000).toLocaleTimeString(); 
+            sunset.innerHTML = new Date(response.sys.sunset * 1000).toLocaleTimeString();  
+        })
+        .catch(err => console.error('Error:', err));
 }
 
-submit.addEventListener("click", (e)=>{
-	e.preventDefault()
-	getWeather(city.value)
-})
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather(city.value); 
+});
 
-getWeather("Delhi")
-
-const cities = ["Delhi", "Shanghai", "New York", "Tokyo"];
-
-        const updateWeatherTable = (city) => {
-            fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-                .then(response => response.json())
-                .then(response => {
-                    console.log(response);
-                    const weatherTable = document.getElementById('weatherTable').getElementsByTagName('tbody')[0];
-                    const tableRow = document.createElement('tr');
-                    tableRow.innerHTML = `
-                        <td>${city}</td>
-                        <td>${response.cloud_pct}</td>
-                        <td>${response.temp}</td>
-                        <td>${response.feels_like}</td>
-                        <td>${response.humidity}</td>
-                        <td>${response.min_temp}</td>
-                        <td>${response.max_temp}</td>
-                        <td>${response.wind_speed}</td>
-                        <td>${response.sunrise}</td>
-                        <td>${response.sunset}</td>
-                    `;
-                    weatherTable.appendChild(tableRow);
-                })
-                .catch(err => console.error(err));
-        }
-
-        // Fetch and update weather details for predefined cities
-        cities.forEach(city => {
-            updateWeatherTable(city);
-        });
+getWeather("Delhi");
